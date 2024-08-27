cbdc971833c0f888beaa4f507b5608da005b727b
Barbara Cavalcante <bcavalcante.babi@gmail.com>
2024-08-27 19:40:45 -0300

    atividade 3

--- src/app/nomes/page.js
+++ src/app/nomes/page.js
@@ -1,32 +1,23 @@
 'use client'
 
-import React from 'react';
-import Cabecalho from '../components/Cabecalho';
-import { Alert, Container } from 'react-bootstrap';
-import Link from 'next/link'
+
+import { useState } from 'react'
+import { Button } from 'react-bootstrap'
 import Pagina from '../components/Pagina';
 
+
 export default function Nomes() {
-  
 
+    const [nome, setNome] = useState("Barbara");
+
+    function alterarNome(){
+      const novoNome = nome == 'Barbara' ? 'Barbara Cavalcante' : 'Barbara'
+      setNome(novoNome)
+    }
     return (
-        <>
         <Pagina titulo="Nomes">
-            <Cabecalho titulo="Nomes" />
-        
-            <Container>
-                <Alert>
-                    Atenção! Preste muita atenção.
-                </Alert>
-
-                <Link href="/">Página Inicial</Link><br />
-                <Link href="/clientes">Página Clientes</Link><br />
-
-                <h1>Fundamentos</h1>
-                <p>Sucesso</p>
-                <Cabecalho />
-            </Container>
+            <h1>{nome}</h1>
+            <Button onClick={alterarNome}>Alterar Nome</Button>
         </Pagina>
-        </>
     )
 }
