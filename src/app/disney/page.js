ed356c8414b6fdbd16ad849b868b08d30745000e
Barbara Cavalcante <bcavalcante.babi@gmail.com>
2024-09-02 19:36:50 -0300

    api

---
+++ src/app/disney/page.js
@@ -0,0 +1,26 @@
+
+'use client'
+
+
+import { useEffect, useState } from "react";
+import apiDisney from "../services/apiDisney";
+import Pagina from "../components/Pagina";
+
+
+export default function Page(){
+    const [personagens, setPersonagens] = useState([])
+    useEffect(()=>{
+        apiDisney.get('character').then(resultado=>{
+            setPersonagens(resultado.data.data)
+        })
+    }, [])
+    
+    return (
+        <Pagina titulo="Disney">
+            {personagens.map(item => (
+                <p>{item.name}</p>
+            ))}
+
+        </Pagina>
+    )
+}
